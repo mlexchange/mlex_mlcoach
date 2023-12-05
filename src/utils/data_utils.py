884b68116a2360d35ba805912323a8a4c9905ab6
taxe10 <tanchavez@lbl.gov>
2023-12-04 21:08:29 -0800

    load already downloaded tiled data

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -28,14 +28,20 @@ def prepare_directories(user_id, data_project, project_id, pattern = r'[/\\?%*:|
     for dataset in data_project.data:
         uri_list.append(dataset.uri)
         uid_list.append(str(uuid.uuid4()))
-    data_info = pd.DataFrame({'uri': uri_list, 'type': ['file']*len(data_project.data)})
+    data_info = pd.DataFrame({'uri': uri_list})
     if data_project.data[0].type == 'tiled':
+        data_info['type'] = ['tiled']*len(data_project.data)
         cleaned_project_id = re.sub(pattern, '_', project_id)               # clean project_id
         local_path = pathlib.Path(f'data/tiled_local_copy/{cleaned_project_id}')
-        data_info['local_uri'] = [f'{local_path}/{uid}.tif' for uid in uid_list]
         if not local_path.exists():
+            data_info['local_uri'] = [f'{local_path}/{uid}.tif' for uid in uid_list]
             local_path.mkdir(parents=True)
             data_project.tiled_to_local_project(project_id, data_info['local_uri'])
+            data_info.to_parquet(f'{local_path}/data_info.parquet', engine='pyarrow')
+        else:
+            data_info = pd.read_parquet(f'{local_path}/data_info.parquet', engine='pyarrow')
+    else:
+        data_info['type'] = ['file']*len(data_project.data)
     data_info.to_parquet(f'{out_path}/data_info.parquet', engine='pyarrow')
     return experiment_id, out_path, f'{out_path}/data_info.parquet'
 
